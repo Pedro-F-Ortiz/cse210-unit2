4b18a94f888de47c65cfe45bbca3d8c0185f2d8c
FIRST_NAME LAST_NAME <jschro678@gmail.com>
2022-10-06 15:22:19 -0600

    random

--- Game/Director.cs
+++ Game/Director.cs
@@ -4,20 +4,18 @@ namespace HiloGame
 {
     public class Director
     {
+        bool isPlaying = true;
 
-        bool isPlaying = true; 
+        Card myCard = new Card();
 
-        Card myCard = new Card(); 
-        
         Card nextCard = new Card();
 
         Card intScore = new Card();
 
         public Director()
         {
-            int current_card = myCard.pick_card ();
+            int current_card = myCard.pick_card();
         }
-        
 
         public void StartGame()
         {
@@ -27,25 +25,18 @@ namespace HiloGame
                 doUpdates();
                 getRender();
             }
-        }  
+        }
 
         public void getInputs(int current_card)
         {
             Console.WriteLine($"This card is: {current_card}");
-            Console.Write("Higher or Lower? [y/n]" );
+            Console.Write("Higher or Lower? [y/n]");
             string guess_card = Console.ReadLine();
-        }  
-
-
-// Vin. Everything is completed up until the input. Complete the last 2 methods. Text us if you need help. 
-        public void doUpdates()
-        {
-
-        }
-        public void getRender()
-        {
-
         }
 
+        // Vin. Everything is completed up until the input. Complete the last 2 methods. Text us if you need help.
+        public void doUpdates() { }
+
+        public void getRender() { }
     }
 }
